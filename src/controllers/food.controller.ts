20249a65e643eb81f8b7fa17c8f569e0523cfeb1
NixBaBy <tuuguu123123@gmail.com>
2025-03-12 12:41:38 +0800

    change

--- src/controllers/food.controller.ts
+++ src/controllers/food.controller.ts
@@ -4,16 +4,25 @@ import { Request, Response } from "express";
 export const createFood = async (req: Request, res: Response) => {
   try {
     const foodData = req.body;
-    const newFood = await foodModel.create(foodData);
+    console.log(foodData);
+    const newFood = await foodModel.create({
+      foodName: foodData.foodName,
+      price: foodData.foodPrice,
+      image:
+        "https://res.cloudinary.com/dhvtog3m2/image/upload/v1741750194/55bc133b8044d28cf937929f2e9f99ce_mu4jcd.png",
+      ingredients: foodData.Ingredients,
+      category: foodData.category,
+    });
     res.status(200).json({ message: "Successfully created category", newFood });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "error in createFood", error });
   }
 };
 
 export const getFood = async (req: Request, res: Response) => {
   try {
-    const newFood = await foodModel.find();
+    const newFood = await foodModel.find().populate("category");
     res.status(200).json({ message: "Successfully created category", newFood });
   } catch (error) {
     res.status(500).json({ message: "error in getFood", error });
